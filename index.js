6c3f86c6dbf1150661672be8b86b3e1f4d9c3ebd
saisrivathsa <srivathsaeric@gmail.com>
2017-06-25 01:57:12 +0530

    Added user create

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var express = require('express');
-var app = express()
+var app = express();
+var mysql = require('mysql');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var timesyncServer = require('timesync/server');
 
 var Room = require('./scripts/room');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "hello",
+  database: "musicroom"
+});
+
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use('timesync', timesyncServer.requestHandler);
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(8080, function(){
+    console.log('listening on *:8080');
 });
 
 Room.io = io;
@@ -26,7 +35,7 @@ app.get('/room/:id', function(req, res){
 });
 
 app.get('/create/room', function(req, res){
-    res.render('create');
+    res.render('usercreate');
 });
 
 app.get('/account/', function(req, res){
@@ -56,3 +65,37 @@ io.on('connection', function(socket){
         console.log("Opening the song link");
     });
 });
+
+var JSON = require('JSON');
+app.post('/usercreate', function(req, res){
+   
+    if (req.method == 'POST') {
+       console.log("hello");
+
+        con.connect(function(err) {
+        if (err) throw err;
+        var body = "";
+        req.on('data',function(data){
+          body +=data;
+        });
+        var data = {};
+        var i = '';
+        var j = '';
+        var k = '';
+        req.on('end', function () {
+        body = body.split("&");
+        is = body[0].split("=")[1];
+        js = body[1].split("=")[1];
+        ks = body[2].split("=")[1];
+        var sql = 'INSERT INTO user (username, email, password) VALUES ("'+is+'","'+js+'","'+ ks+'")';
+          con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            res.redirect('/');
+
+          }); 
+      });
+        });   
+        }
+
+    });
